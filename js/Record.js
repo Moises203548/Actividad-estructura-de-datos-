e2f5b631b489449f510e2201e2789ca6b537e1ee
Moises203548 <mortegas4@unicartagena.edu.co>
2026-09-14 11:51:29 -0500

    feat(js): inicializacion de 3 instancias por tipo

--- js/Record.js
+++ js/Record.js
@@ -1,3 +1,8 @@
 function crearEstudianteRecord(nombre, edad, promedio) {
     return { nombre, edad, promedio };
 }
+const estudiantes = [
+    crearEstudianteRecord("Ana Torres", 20, 4.2),
+    crearEstudianteRecord("Luis Pérez", 22, 3.8),
+    crearEstudianteRecord("Marta Ruiz", 21, 4.5),
+];
